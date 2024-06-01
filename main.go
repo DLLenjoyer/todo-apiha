9a806904bbf3fcd09d1df8db26831663241fe2bf
0x2170 <151378415+DLLenjoyer@users.noreply.github.com>
2024-06-01 16:30:37 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/DLLenjoyer/todo-apiha/handlers"
+	"github.com/DLLenjoyer/todo-apiha/models"
+	"github.com/DLLenjoyer/todo-apiha/repository"
+	"github.com/DLLenjoyer/todo-apiha/service"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	repo := repository.InMemoryTodoRepository{
+		Todos: []models.Todo{},
+	}
+
+	todoService := service.NewTodoService(&repo)
+
+	todoHandler := handlers.NewTodoHandler(todoService)
+
+	r := mux.NewRouter()
+
+	todoHandler.SetupRoutes(r)
+
+	log.Println("starting server on :6030")
+	log.Fatal(http.ListenAndServe(":6030", r))
+}
